f67e97fee39ed5f52cd4c5110de06b60aabcd5a4
ChristianPucci91 <pucci.christian91@gmail.com>
2021-01-28 15:23:58 +0100

    TEST creazione tabella Employees funzionante - esercizio completato

--- database280121/database/seeds/DatabaseSeeder.php
+++ database280121/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
           BrandSeeder::class,
           HeadquarterSeeder::class,
+          EmployeeSeeder::class,
         ]);
     }
 }
